6318ba66da98234def5acfcf2f6365b9b4e9bfe0
Will Cheng <chengw@cheyenne1.ib0.cheyenne.ucar.edu>
2021-01-29 18:24:27 -0700

    added centennial build

---
+++ build/centennial_params.sh
@@ -0,0 +1,10 @@
+
+# excalibur specific env vars
+export BASEDIR="/p/work2"
+export USER="atec4dwx"
+export ACCOUNT_KEY="-A DPGUT38332XWX"
+export QUEUE="-q xwx"
+export DSP="-l dsp=xwx"
+export MPICMD="aprun"
+export DO_TAR_SUM_FOR_DISTRIB="0"
+
